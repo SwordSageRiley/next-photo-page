500ea0373a1a3cbbad5a388cbb1427dd2fd99738
Kira <riley@cl4p-tp.com>
2025-01-08 22:26:36 -0800

    messing with react packages for slideshow. still getting errors. commented out the errors for now

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -1,15 +1,56 @@
+
+import { useState } from 'react';
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import '@/app/css/gallery.css';
 
 export function Gallery(props) {
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        }
+    };
+    const [slide, setSlide] = useState(false);
 
+    function picClick(id){
+        const car = document.getElementsByClassName('caro');
+        console.log(car);
+       // car[0].goToSlide(id);
+    }
 
     return (
-        <div className="gallery">
-            {props.pics.map((pic: string) => (
-                <div className='galbox'>
-                    <img src={pic} className='galpic' alt='fix this' />
-                </div>
-            ))}
+        <div className="gal">
+            <div className='gallery'>
+                {props.pics.map((pic) => (
+                    <div key={pic.id} className='gal-box' onClick={() => picClick(pic.id)}>
+                        <img src={pic.url} className='gal-pic' alt={pic.alt} />
+                    </div>
+                ))}
+            </div>
+
+            <Carousel responsive={responsive}
+                infinite={true}
+                ssr={true}
+                className={`${slide ? '' : 'slideOff'} caro`}
+                centerMode={false}
+                >
+                {props.pics.map((pic) => (
+                    <div key={pic.id} className='slideshow-box'>
+                        <img src={pic.url} className='slideshow-pic' alt={pic.alt} />
+                    </div>
+                ))}
+            </Carousel>
         </div>
     );
 }
--- app/wildlife/page.tsx
+++ app/wildlife/page.tsx
@@ -9,33 +9,54 @@ export default function Wildlife(){
         {
             id: 1,
             url: 'https://i.imgur.com/SIWDItQ.jpeg',
-            alt: ''
+            alt: 'Photo of a swan on a lake with reeds in the background.'
         },
         {
             id: 2,
             url: 'https://i.imgur.com/04w37e1.jpeg',
-            alt: ''
+            alt: 'Photo of a swan reflected on a lake.'
         },
         {
             id: 3,
             url: 'https://i.imgur.com/TfWhrEx.jpeg',
-            alt: ''
+            alt: 'Photo of a swan reflected on a lake with a stone wall in the background.'
         },
         {
             id: 4,
             url: 'https://i.imgur.com/bobshri.jpeg',
-            alt: ''
+            alt: 'Photo of a beaver swimming.'
         },
         {
             id: 5,
             url: 'https://i.imgur.com/wjLZTAa.jpeg',
-            alt: ''
+            alt: 'Photo of a hawk in a tree.'
         },
         {
             id: 6,
             url: 'https://i.imgur.com/0tu3FQQ.jpeg',
-            alt: ''
+            alt: 'Photo of a hawk in a tree.'
         },
+        {
+            id: 7,
+            url: 'https://i.imgur.com/pCsGjT7.jpeg',
+            alt: 'Photo of a red tailed hawk standing in dried grass.'
+        },
+        {
+            id: 8,
+            url: 'https://i.imgur.com/aUCzPyk.jpeg',
+            alt: 'Photo of a red tailed hawk standing over a snake it had caught.'
+        },
+        {
+            id: 9,
+            url: 'https://i.imgur.com/i10bstt.jpeg',
+            alt: 'Photo of a hawk flying, carrying a snake it had caught.'
+        },
+        {
+            id: 10,
+            url: 'https://i.imgur.com/rhDH12a.jpeg',
+            alt: 'Photo of a dragonfly on a rock.'
+        },
+        
 
     ]
 
@@ -46,11 +67,3 @@ export default function Wildlife(){
         </div>
     );
 }
-
-
-/*
-        'https://i.imgur.com/pCsGjT7.jpeg',
-        'https://i.imgur.com/aUCzPyk.jpeg',
-        'https://i.imgur.com/i10bstt.jpeg',
-        'https://i.imgur.com/rhDH12a.jpeg'
- */
