c89c2c8f2452724f68bda46102e9c52f90300f68
Kira <riley@cl4p-tp.com>
2025-02-27 12:15:54 -0800

    updating imports to try and handle build error

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ import localFont from 'next/font/local';
 
 import { Banner } from "./components/banner";
 import { Nav } from '@/app/components/nav';
+import { About } from "./components/about2";
 // import About from './components/about';
-import { Toggles } from '@/app/components/toggles';
-import { Highlight } from "@/app/components/highlight";
+// import { Toggles } from '@/app/components/toggles';
+// import { Highlight } from "@/app/components/highlight";
 
 const myFont = localFont({ src: '../public/avali-scratch-rus-romanized.ttf' });
 
@@ -17,12 +18,12 @@ export default function Home() {
   const [avali, setAvali] = useState(false);
   const [theme, setTheme] = useState(true);
 
-  function changeTheme() {
-    setTheme(!theme);
-  }
-  function changeAvali() {
-    setAvali(!avali);
-  }
+  // function changeTheme() {
+  //   setTheme(!theme);
+  // }
+  // function changeAvali() {
+  //   setAvali(!avali);
+  // }
 
 
   return (
@@ -30,7 +31,7 @@ export default function Home() {
       <Banner />
       <Nav />
       {/* <Highlight /> */}
-      {/* <About /> */}
+      <About />
       {/* <Toggles avaliToggle={changeAvali} darkToggle={changeTheme} /> */}
     </div>
   );
